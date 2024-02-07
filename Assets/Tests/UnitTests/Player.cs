a892814846b8edfe520f5ae28a9b32826af51062
Love Friberger <love.friberger@starstable.com>
2024-02-07 22:57:56 +0100

    Shoot take damage and spawn test

--- Assets/Tests/UnitTests/Player.cs
+++ Assets/Tests/UnitTests/Player.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 using Zenject;
 using NUnit.Framework;
+using UnityEngine.AddressableAssets;
+using UnityEditor;
+using UnityEngine.TestTools;
+using System.Collections;
 
 [TestFixture]
 public class Player : ZenjectUnitTestFixture
@@ -11,14 +15,31 @@ public class Player : ZenjectUnitTestFixture
         var rigidbody = new GameObject().AddComponent<Rigidbody2D>();
         Container.Bind<PlayerModel>().AsSingle().WithArguments(rigidbody, rigidbody.transform);
 
+        Container.Bind<GameManagerModel>().AsSingle();
+        Container.Bind<GameManagerController>().AsSingle();
+        var spawnerGameObject = new GameObject().AddComponent<AssetReferenceSpawnerObject>();
+        Container.Inject(spawnerGameObject);
+        Container.BindInstance(spawnerGameObject).AsSingle();
+        Container.Bind<AssetReferenceSpawner>().AsSingle().WithArguments(spawnerGameObject);
         Container.Bind<PlayerMover.Settings>().AsSingle();
 
         Container.Bind<InputModel>().AsSingle();
         Container.Bind<InputView>().AsSingle();
 
         Container.BindInterfacesAndSelfTo<PlayerMover>().AsSingle();
+
+        var projectile = new AssetReferenceGameObject(AssetDatabase.AssetPathToGUID("Assets/Prefabs/Projectile.prefab"));
+
+        Container.Bind<PlayerShooter.Settings>().AsSingle().OnInstantiated((i, o) => { (o as PlayerShooter.Settings).projectilePrefabReference = projectile; });
         Container.BindInterfacesAndSelfTo<PlayerShooter>().AsSingle();
+
+        var transform = new GameObject().transform;
+        Container.BindInstance(transform);
+        var player = new AssetReferenceGameObject(AssetDatabase.AssetPathToGUID("Assets/Prefabs/Player.prefab"));
+        Container.Bind<PlayerSpawner.Settings>().AsSingle().OnInstantiated((i, o) => { (o as PlayerSpawner.Settings).playerPrefabReference = player; });
         Container.Bind<PlayerSpawner>().AsSingle();
+
+        Container.Bind<PlayerDamageTaker>().AsSingle();
 
         Container.Inject(this);
     }
@@ -29,6 +50,16 @@ public class Player : ZenjectUnitTestFixture
     InputModel inputModel;
     [Inject]
     PlayerModel playerModel;
+    [Inject]
+    PlayerShooter playerShooter;
+    [Inject]
+    AssetReferenceSpawnerObject assetReferenceSpawnerObject;
+    [Inject]
+    PlayerDamageTaker playerDamageTaker;
+    [Inject]
+    GameManagerController gameManagerController;
+    [Inject]
+    PlayerSpawner playerSpawner;
 
     [Test]
     public void Turn()
@@ -49,5 +80,48 @@ public class Player : ZenjectUnitTestFixture
         inputModel.rightInputHold = false;
     }
 
-    
+    [UnityTest]
+    public IEnumerator Shoot()
+    {
+        Assert.True(assetReferenceSpawnerObject.transform.childCount == 0);
+
+        inputModel.inputType = InputModel.Type.Player;
+
+        inputModel.actionInputDown = true;
+        playerShooter.Tick();
+
+        inputModel.actionInputDown = false;
+        //Wait some frames to make sure the asset is loaded
+        for (int i = 0; i < 500; i++)
+            yield return null;
+
+        Assert.True(assetReferenceSpawnerObject.transform.childCount > 0);
+        var projectile = assetReferenceSpawnerObject.transform.GetChild(0).GetComponent<Projectile>();
+        Assert.True(projectile != null);
+        Assert.True(projectile.GetComponent<Rigidbody2D>().velocity.sqrMagnitude > 0);
+        Object.DestroyImmediate(projectile);
+    }
+
+    [Test]
+    public void TakeDamage()
+    {
+        var playerKilled = false;
+        gameManagerController.AddPlayerKilledAction(() => playerKilled = true);
+        playerDamageTaker.TakeDamage();
+        Assert.IsTrue(playerKilled);
+    }
+
+    [UnityTest]
+    public IEnumerator Spawner()
+    {
+        Assert.True(assetReferenceSpawnerObject.transform.childCount == 0);
+        playerSpawner.Initialize();
+
+        //Wait some frames to make sure the asset is loaded
+        for (int i = 0; i < 500; i++)
+            yield return null;
+
+        Assert.True(assetReferenceSpawnerObject.transform.childCount == 1);
+        Assert.True(assetReferenceSpawnerObject.transform.GetChild(0).GetComponent<PlayerObject>() != null);
+    }
 }
