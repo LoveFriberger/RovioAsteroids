0dd23cd054880cba3b2efe9e74c4308daa460e08
Love Friberger <love.friberger@starstable.com>
2024-02-09 12:42:31 +0100

    Moved tests from unit tests to play mode tests

--- Assets/Tests/UnitTests/Player.cs
+++ Assets/Tests/UnitTests/Player.cs
@@ -25,7 +25,7 @@ public class Player : ZenjectUnitTestFixture
         var spawnerGameObject = new GameObject().AddComponent<AssetReferenceSpawnerObject>();
         Container.Inject(spawnerGameObject);
         Container.BindInstance(spawnerGameObject).AsSingle();
-        Container.Bind<AssetReferenceSpawner>().AsSingle().WithArguments(spawnerGameObject);
+        Container.Bind<AssetReferenceSpawner>().AsTransient().WithArguments(spawnerGameObject);
 
         Container.Bind<PlayerModel>().AsSingle().WithArguments(rigidbody, rigidbody.transform);
         Container.Bind<PlayerMover.Settings>().AsSingle();
@@ -34,7 +34,11 @@ public class Player : ZenjectUnitTestFixture
         Container.Bind<PlayerDamageTaker>().AsSingle();
         Container.BindInterfacesAndSelfTo<PlayerMover>().AsSingle();
 
-        Container.Bind<PlayerShooter.Settings>().AsSingle().OnInstantiated((i, o) => { (o as PlayerShooter.Settings).projectilePrefabReference = projectile; });
+        Container.Bind<PlayerShooter.Settings>().AsSingle().OnInstantiated((i, o) => {
+            var settings = (o as PlayerShooter.Settings);
+            settings.projectilePrefabReference = projectile;
+            settings.cooldown = -5;
+        });
         Container.BindInterfacesAndSelfTo<PlayerShooter>().AsSingle();
 
         Container.Bind<PlayerSpawner.Settings>().AsSingle().OnInstantiated((i, o) => { (o as PlayerSpawner.Settings).playerPrefabReference = player; });
@@ -49,15 +53,9 @@ public class Player : ZenjectUnitTestFixture
     [Inject]
     PlayerModel playerModel;
     [Inject]
-    PlayerShooter playerShooter;
-    [Inject]
-    AssetReferenceSpawnerObject assetReferenceSpawnerObject;
-    [Inject]
     PlayerDamageTaker playerDamageTaker;
     [Inject]
     GameManagerController gameManagerController;
-    [Inject]
-    PlayerSpawner playerSpawner;
 
     [Test]
     public void Turn()
@@ -77,29 +75,8 @@ public class Player : ZenjectUnitTestFixture
         Assert.True(playerModel.Rotation.eulerAngles.z < 40);
         inputModel.rightInputHold = false;
     }
-
-    [UnityTest]
-    public IEnumerator Shoot()
-    {
-        Assert.True(assetReferenceSpawnerObject.transform.childCount == 0);
-
-        inputModel.inputType = InputModel.Type.Player;
-        inputModel.actionInputDown = true;
-        playerShooter.Tick();
-        inputModel.actionInputDown = false;
-
-        //Wait some frames to make sure the asset is loaded
-        for (int i = 0; i < 500; i++)
-            yield return null;
-
-        Assert.True(assetReferenceSpawnerObject.transform.childCount > 0);
-        var projectile = assetReferenceSpawnerObject.transform.GetChild(0).GetComponent<Projectile>();
-        Assert.True(projectile != null);
-        Assert.True(projectile.GetComponent<Rigidbody2D>().velocity.sqrMagnitude > 0);
-
-        Object.DestroyImmediate(projectile);
-    }
-
+    
+    
     [Test]
     public void TakeDamage()
     {
@@ -107,22 +84,5 @@ public class Player : ZenjectUnitTestFixture
         gameManagerController.AddPlayerKilledAction(() => playerKilled = true);
         playerDamageTaker.TakeDamage();
         Assert.IsTrue(playerKilled);
-    }
-
-    [UnityTest]
-    public IEnumerator Spawner()
-    {
-        Assert.True(assetReferenceSpawnerObject.transform.childCount == 0);
-        playerSpawner.Initialize();
-
-        //Wait some frames to make sure the asset is loaded
-        for (int i = 0; i < 500; i++)
-            yield return null;
-
-        Assert.True(assetReferenceSpawnerObject.transform.childCount == 1);
-        var playerObject = assetReferenceSpawnerObject.transform.GetChild(0).GetComponent<PlayerObject>();
-        Assert.True(assetReferenceSpawnerObject.transform.GetChild(0).GetComponent<PlayerObject>() != null);
-
-        Object.DestroyImmediate(playerObject);
     }
 }
